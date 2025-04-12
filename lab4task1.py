ea14ebf7b4b262725cff4b3a22b8ea3966f8db2b
ttossshaa <ttossshaa@gmail.com>
2025-04-12 23:36:45 +0300

    Add files via upload

---
+++ lab4task1.py
@@ -0,0 +1,19 @@
+from Bio import SeqIO
+
+def calculate_gc_content(sequence):
+    gc_count = sequence.count('G') + sequence.count('C') #Подсчитывает количество гуанинов (G) и цитозинов (C) в последовательности 
+    return gc_count / len(sequence)
+
+def sort_records_by_gc(gb_file):
+    records = [] #пустой список
+    for record in SeqIO.parse(gb_file, "genbank"):
+        gc_content = calculate_gc_content(str(record.seq).upper()) #upper преобразует строку в верхний регистр
+        records.append((record, gc_content))
+
+    records.sort(key=lambda x: x[1])#указывает, что сортировка должна выполняться на основе второго элемента каждого кортежа
+
+    for record, gc_content in records:
+        print(f"{record.id}: {record.description}, GC = {gc_content}")
+
+file_path = "C:\\python\\sequence.gb"
+sort_records_by_gc(file_path)   
